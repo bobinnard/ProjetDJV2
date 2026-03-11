3668d1c78a1dda14660c85de6a23be26be4ecc53
PaquitoPaquito <matthieu.lesbros@gmail.com>
2026-03-11 16:58:24 +0100

    Implemented tower placement

--- Assets/Scripts/UI/TowerPosition.cs
+++ Assets/Scripts/UI/TowerPosition.cs
@@ -1,32 +1,43 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TowerPosition : MonoBehaviour
 {
-    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject towerMenu;
     [SerializeField] private Button button;
 
-    private bool _isTowerMenuActive = false;
-    private bool _isOccupied = false;
+    private bool _isTowerMenuActive;
+    private bool _isOccupied;
+    private bool _isSelected;
 
-    private void Start()
+    private void Update()
     {
-        
+        if (_isSelected && EventSystem.current.currentSelectedGameObject != button.gameObject) _isSelected = false;
     }
-    
-    private void ToggleMenu()
+        
+    public void ToggleMenu()    
     {
         if (!_isOccupied)
         {
+            Debug.Log("1");
+            if (_isSelected)
+            {
+                Debug.Log("2");
+                BuildTower();
+            }
+            else _isSelected = true;
+            return;
         }
-        else _isTowerMenuActive = !_isTowerMenuActive;
-        menu.SetActive(_isTowerMenuActive);
-        return;
-        
+        Debug.Log("3");
+        _isTowerMenuActive = !_isTowerMenuActive;
+        towerMenu.SetActive(_isTowerMenuActive);
     }
 
-    public void BuildTower()
+    private void BuildTower()
     {
         _isOccupied = true;
+        var towerToBuild = Instantiate(ShopManager.Instance.towerTypesPrefabs[ShopManager.Instance.selectedTowerType],transform);
+        towerToBuild.SetActive(true);
     }
 }
