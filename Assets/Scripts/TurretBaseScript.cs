3a82b6ed4d48fc893aa01e07f9fb3650c08ec3e5
PaquitoPaquito <matthieu.lesbros@gmail.com>
2026-03-20 09:52:17 +0100

    Implementing tower attack animations

--- Assets/Scripts/TurretBaseScript.cs
+++ Assets/Scripts/TurretBaseScript.cs
@@ -6,18 +6,19 @@ public class TurretBaseScript : MonoBehaviour
 {
     [SerializeField] public TurretInfo info;
     [SerializeField] private GameObject mesh;
+    [SerializeField] private GameObject bulletMesh;
     protected float range;
     protected int damage;
     protected float attackSpeed;
     protected int Cost;
-    protected int[] UpgradeCost;
     protected int level = 0;
+    private int[] _upgradeCost;
 
     // returns bool indicating if upgrade was successful
     public virtual bool Upgrade(){
         // We check whether the player has enough money to upgrade
-        if (level < UpgradeCost.Length && GameManager.Instance.money >= UpgradeCost[level]){
-            GameManager.Instance.money -= UpgradeCost[level];
+        if (level < _upgradeCost.Length && GameManager.Instance.money >= _upgradeCost[level]){
+            GameManager.Instance.money -= _upgradeCost[level];
             // We upgrade, the heritage manages the stat changes
             level++;
             // We raise the mesh to visually show the upgrade
@@ -28,12 +29,47 @@ public class TurretBaseScript : MonoBehaviour
         return false;
     }
     
-    void Awake()
+    private void Awake()
     {
         range = info.range;
         damage = info.damage;
         attackSpeed = info.attackSpeed;
         Cost = info.Cost;
-        UpgradeCost = info.UpgradeCost;
+        _upgradeCost = info.UpgradeCost;
+    }
+
+    // purely visual effect, entirely hard coded
+    IEnumerator AnimateAttack(Transform target)
+    {
+        var pos = transform.position + new Vector3(0.5f, Mathf.Max(level, 2) * 0.3f + 0.5f, 0.5f);
+        var bullet = Instantiate(bulletMesh, pos, Quaternion.identity);
+        bullet.SetActive(true);
+        bullet.transform.localScale = Vector3.zero;
+        float time = 0;
+        // appear
+        while (time < 0.25f)
+        {   
+            bullet.transform.localScale = time * 4 * Vector3.one;
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        time = 0;
+        // turn faster and faster
+        while (time < 0.25f)
+        {
+            bullet.transform.rotation = Quaternion.AngleAxis(Mathf.Exp(time * 4) - 1, Vector3.up);
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        time = 0;
+        // attack
+        var direction = target.position - bullet.transform.position;
+        while (time < 0.5f)
+        {
+            bullet.transform.rotation = Quaternion.AngleAxis(time*100, Vector3.up);
+            bullet.transform.position = pos + direction * time * 2;
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
