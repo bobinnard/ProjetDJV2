6cfe46d8b7f8232f999bbdaf4e31a43a3d07db36
bobinnard <skyland1360@gmail.com>
2026-03-20 09:57:07 +0100

    continued tests

--- Assets/Tests/TowersTesting.cs
+++ Assets/Tests/TowersTesting.cs
@@ -47,5 +47,47 @@ public class TowersTesting
         Assert.That(GameManager.Instance.aliveEnnemies <= 0);
         Assert.That(GameManager.Instance.score >= 0);
         Assert.That(GameManager.Instance.isInBuildPhase == true);
+        request = Addressables.LoadAsset<GameObject>("Assets/Prefab/Ennemies/Ennemy3.prefab");
+        yield return request;
+        GameObject ennemy1 = GameObject.Instantiate(request.Result,liien.transform.position+new Vector3(1,0,0),Quaternion.identity);
+        GameObject ennemy2 = GameObject.Instantiate(request.Result,liien.transform.position+new Vector3(1,0,0),Quaternion.identity);
+        GameObject ennemy3 = GameObject.Instantiate(request.Result,liien.transform.position+new Vector3(1,0,0),Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Assert.That(GameManager.Instance.aliveEnnemies <= -3);
+    }
+
+    [UnityTest]
+    public IEnumerator PXLTest()
+    {
+        var request = Addressables.LoadAsset<GameObject>("Assets/Prefab/Turrets/Liien.prefab");
+        yield return request;
+        GameObject liien = GameObject.Instantiate(request.Result,Vector3.forward,Quaternion.identity);
+        liien.transform.LookAt(Vector3.zero);
+        Liien liienCode;
+        if(liien.TryGetComponent<Liien>(out liienCode))
+        {
+            Assert.That(liienCode.VerifyValues(1,5,1));
+            liienCode.Upgrade();
+            Assert.That(liienCode.VerifyValues(1,10,0.8f));
+            liienCode.Upgrade();
+            Assert.That(liienCode.VerifyValues(1,20,0.6f));
+            liienCode.Upgrade();
+            Assert.That(liienCode.VerifyValues(2,20,0.6f));
+        } 
+        GameManager.Instance.isInBuildPhase = false;
+        for (int i = 0; i<5; i++){
+            GameManager.Instance.LaunchNextRound();
+            yield return new WaitForSeconds(10f);
+            Assert.That(GameManager.Instance.aliveEnnemies <= 0);
+            Assert.That(GameManager.Instance.score >= 0);
+            Assert.That(GameManager.Instance.isInBuildPhase == true);
+        }
+        request = Addressables.LoadAsset<GameObject>("Assets/Prefab/Ennemies/Ennemy3.prefab");
+        yield return request;
+        GameObject ennemy1 = GameObject.Instantiate(request.Result,liien.transform.position+new Vector3(1,0,0),Quaternion.identity);
+        GameObject ennemy2 = GameObject.Instantiate(request.Result,liien.transform.position+new Vector3(1,0,0),Quaternion.identity);
+        GameObject ennemy3 = GameObject.Instantiate(request.Result,liien.transform.position+new Vector3(1,0,0),Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Assert.That(GameManager.Instance.aliveEnnemies <= -3);
     }
 }
